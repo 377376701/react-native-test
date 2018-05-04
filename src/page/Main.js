41151d52eec7e90968d405ab1267c585808170e0
keke <240977139@qq.com>
2018-05-04 15:44:57 +0800

    201805041544

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,74 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
 
-import React, {Component} from 'react'
 import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    PixelRatio,
-    Alert,
-    Navigator,
-    ScrollView,
-    TouchableHighlight,
-    TouchableOpacity,
+    Image, View,
 } from 'react-native';
 
-type Props = {};
+//引入react-navigation依赖库
+import {
+    TabNavigator,
+} from 'react-navigation';
 
-class Main extends Component<Props> {
+//展示的页面
+import Home from './Home';
+import Type from './Type';
+import Mine from './Mine';
 
-    static navigationOptions = ({navigation, screenProps}) => ({
 
-            headerTitle: navigation.state.params.key,
-            //设置滑动返回的距离
-            gestureResponseDistance: {horizontal: 300},
-            //是否开启手势滑动返回，android 默认关闭 ios打开
-            gesturesEnabled: true,
-            //设置跳转页面左侧返回箭头后面的文字，默认是上一个页面的标题
-            headerBackTitle: null,
-            //导航栏的样式
-            headerStyle: styles.headerStyle,
-            //导航栏文字的样式
-            headerTitleStyle: styles.headerTitleStyle,
-            //返回按钮的颜色
-            headerTintColor: 'white',
-            //隐藏顶部导航栏
-            // header: null,
-            //设置顶部导航栏右边的视图  和 解决当有返回箭头时，文字不居中
-            headerRight: (<View/>),
-            //设置导航栏左边的视图
-            // headerLeft: (<View/>),
+//Tab
+export default Tab = TabNavigator({
+    //每一个页面的配置
+    Home: {
+        screen: Home,//当前选项卡加载的页面
+        //配置每一个选项卡的样式
+        navigationOptions: {
+            tabBarLabel: '首页',//显示的标签文字
+            //显示的图片
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../res/ic_home.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}
+                />
+            ),
+        },
+    },
+    Type: {
+        screen: Type,
+        navigationOptions: {
+            tabBarLabel: '分类',
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../res/ic_type.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>
+            ),
         }
-    );
-
-    render() {
-        return (
-            <View style={styles.flex}>
-
-            </View>
-        );
-    }
-}
-
-
-const styles = StyleSheet.create({
-
-    flex: {flex: 1},
-    headerStyle: {
-        backgroundColor: '#4398ff',
     },
-    headerTitleStyle: {
-        color: 'white',
-        //设置标题的大小
-        fontSize: 18,
-        //居中显示
-        alignSelf: 'center',
+    Mine: {
+        screen: Mine,
+        navigationOptions: {
+            tabBarLabel: '我的',
+            tabBarIcon: ({tintColor}) => (
+                <Image
+                    source={require('../res/ic_me.png')}
+                    style={[{height: 24, width: 24}, {tintColor: tintColor}]}/>
+            ),
+        }
     },
+
+}, {
+    //设置TabNavigator的位置
+    tabBarPosition: 'bottom',
+    //是否在更改标签时显示动画
+    animationEnabled: true,
+    //是否允许在标签之间进行滑动
+    swipeEnabled: true,
+    //按 back 键是否跳转到第一个Tab(首页)， none 为不跳转
+    backBehavior: "none",
+    //设置Tab标签的属性
+
+    tabBarOptions: {
+        //Android属性
+        upperCaseLabel: false,//是否使标签大写，默认为true
+        //共有属性
+        showIcon: true,//是否显示图标，默认关闭
+        showLabel: true,//是否显示label，默认开启
+        activeTintColor: '#EB3695',//label和icon的前景色 活跃状态下（选中）
+        inactiveTintColor: 'gray',//label和icon的前景色 活跃状态下（未选中）
+        style: { //TabNavigator 的背景颜色
+            backgroundColor: 'white',
+            height: 55,
+        },
+        indicatorStyle: {//标签指示器的样式对象（选项卡底部的行）。安卓底部会多出一条线，可以将height设置为0来暂时解决这个问题
+            height: 0,
+        },
+        labelStyle: {//文字的样式
+            fontSize: 13,
+            marginTop: -5,
+            marginBottom: 5,
+        },
+        iconStyle: {//图标的样式
+            marginBottom: 5,
+        }
+    },
+
 });
-
-module.exports = Main;
